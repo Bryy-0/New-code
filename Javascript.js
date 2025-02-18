71edaaeec408fb9d78828f5000ee296d679ac363
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-02-18 13:02:02 +0800

    s

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+function button()
+{
+    document.getElementById("java").innerHTML="Complete";
+    //Class is next to getElementById("Class here")
+
+}
+
+function time()
+{
+    document.getElementById('test').innerHTML = Date();
+}
